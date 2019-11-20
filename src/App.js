d3887f8ed8832a37261ef46635fae9f33c2d5388
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-11-20 12:32:23 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://restcountries.eu/rest/v2/regionalbloc/eu'
+      );
+      setCountries(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Oh the places I've been!</h1>
+      
+        {countries.map(country => (
+          <li key={country.name}>
+            <p>{country.name}</p>
+          </li>
+        ))}
     </div>
   );
 }
