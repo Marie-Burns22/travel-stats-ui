038260fbc470c9696922a860d7179f5f714c9b98
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-11-20 17:39:09 -0800

    Clean up code and add comments.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
-
-import './App.css';
+// import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -11,6 +10,7 @@ function App() {
   const [wantToVisit, setWantToVisit] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState({});
 
+  // useEffect hook calls API t0 get countries for a specific region based on region selected by user in form. The selection set state for region and the API call is triggered by changes to the region state. 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
@@ -21,17 +21,17 @@ function App() {
     fetchData();
   }, [region]);
 
+  //handleChange uses selection from form to find a country and set state for selectedCountry. Buttons at bottom of the form use the selectedCountry to add it to the myCountries state array or wantToVisit state array.
   function handleChange(e){
     const countryName = e.target.value;
     const country = countries.find(c => c.name === countryName);
     setSelectedCountry(country);
-    console.log(selectedCountry)
-
   };
 
   return (
     <div className="App">
       <h1>Oh the places I've been!</h1>
+      {/* Radio buttons to select region which triggers API call (uses region name in the URL) */}
       <Form>
           <Form.Group onChange={(event) => setRegion(event.target.id)}>
           <Form.Label>Regions</Form.Label>
@@ -73,12 +73,12 @@ function App() {
         </Form.Group>
       </Form>
       
+      {/* Selection of countries rendered based on region selection. Selected country is saved to state as selectedCountry. Buttons use selectedCountry state to add country object to one of two lists. */}
       <Form>
         <Form.Group controlId="countrySelect">
           <Form.Label>Select Country</Form.Label>
           <Form.Control as="select" onChange={handleChange} multiple>
             {countries.map(country => (
-    
               <option key={country.name} name='country' value={country.name} >{country.name}</option>
             ))}
           </Form.Control>
@@ -94,10 +94,11 @@ function App() {
         </Button>
 
       </Form>
-          <h1>My Countries</h1>
-          {myCountries.map(country => (
-            <p key={country.name}>{country.name}</p>
-          ))}
+      {/* TODO: Need to add delete button to each country rendered in the lists. */}
+      <h1>My Countries</h1>
+        {myCountries.map(country => (
+          <p key={country.name}>{country.name}</p>
+        ))}
 
         <h1>Places I Want To Go!</h1>
         {wantToVisit.map(country => (
