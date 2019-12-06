47f15a98a07ae252185722579dc2a4a14ca74cab
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-12-06 09:23:45 -0800

    Add list of languages to MyTravelStats
    Add function to make an array of language names. Use .join() to create string of names to display.

--- src/MyTravelStats.js
+++ src/MyTravelStats.js
@@ -3,13 +3,21 @@ import {Card, ListGroup} from 'react-bootstrap';
 
 
 export default function MyTravelStats(props){
+    let languageNames = props.languages.map(l => (
+        l.name)
+    );
+    console.log(languageNames);
+
    
     return(
         <Card>
             <Card.Header bg="info" as="h4">My Travel Stats</Card.Header>
             <ListGroup variant="flush">
                 <ListGroup.Item>Total Countries Visited: {props.myCountries.length}</ListGroup.Item>
-                <ListGroup.Item>Total Languages Spoken in All Countries Visited: {props.languages.length}</ListGroup.Item>
+                <ListGroup.Item>
+                    <h5>Total Languages Spoken in All Countries Visited:  {props.languages.length}</h5><br />
+                    <h5>Languages: {languageNames.join(", ")} </h5> 
+                </ListGroup.Item>
                 <ListGroup.Item>Countries I want to visit: {props.wantCount}</ListGroup.Item>
             </ListGroup>
         </Card>
