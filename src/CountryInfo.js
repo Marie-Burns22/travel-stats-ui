9b8ef530f15060110b7d6a49cbcf00d2b94cef26
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-12-26 09:50:53 -0800

    Add no borders message for countries with no borders. Refactor to only show country info card if card is selected.

--- src/CountryInfo.js
+++ src/CountryInfo.js
@@ -10,34 +10,30 @@ export default function CountryInfo({country}) {
     return(
         <Card style={{ marginTop: "1em" }} bg="success">
             <Card.Header as="h4">
-                {!country.name 
-                ?
-                <Card.Title as="h4">Select a Region and Country To See Its Info</Card.Title>
-                :
-                    <Card.Title className="justify-content-between" as="h4">
-                        <Row>
-                        <Col>
-                            {country.name} 
-                        </Col>
-                        <Col>
-                            <Image src={country.flag} style={{height: "2em"}}/>
-                        </Col>
-                        </Row>
-                    </Card.Title>
-                }
+                <Card.Title className="justify-content-between" as="h4">
+                    <Row>
+                    <Col>
+                        {country.name} 
+                    </Col>
+                    <Col>
+                        <Image src={country.flag} style={{height: "2em"}}/>
+                    </Col>
+                    </Row>
+                </Card.Title>
             </Card.Header>
-
-            <ListGroup className="list-group-flush">
-                <ListGroupItem >Languages: {!country.languages ? '' : country.languages.map(language => (
-                    <li key={language.name}>{language.name}</li>
-                    ))} </ListGroupItem>
-                <ListGroupItem>Currencies: {!country.currencies ? '' : country.currencies.map(c => (
-                    <li key={c.name}>{c.name}</li>
-                    ))} </ListGroupItem>
-                <ListGroupItem>Population: {!country.population ? '' : formatNumber(country.population)}</ListGroupItem>
-                <ListGroupItem>Capital: {!country.capital ? '' : country.capital}</ListGroupItem>
-                <ListGroupItem>Borders: {!country.borders ? '' : country.borders.join(", ")}</ListGroupItem>
-            </ListGroup>
+            {!country.name ? null :
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem >Languages: {!country.languages ? '' : country.languages.map(language => (
+                        <li key={language.name}>{language.name}</li>
+                        ))} </ListGroupItem>
+                    <ListGroupItem>Currencies: {!country.currencies ? '' : country.currencies.map(c => (
+                        <li key={c.name}>{c.name}</li>
+                        ))} </ListGroupItem>
+                    <ListGroupItem>Population: {!country.population ? '' : formatNumber(country.population)}</ListGroupItem>
+                    <ListGroupItem>Capital: {!country.capital ? '' : country.capital}</ListGroupItem>
+                    <ListGroupItem>Borders: {country.borders.length === 0 ? 'No land borders' : country.borders.join(", ")}</ListGroupItem>
+                </ListGroup>
+            }
         </Card>
     )
 } 
